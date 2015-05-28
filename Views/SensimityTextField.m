f640c4e6bfea4fa80597cd0af01da22c81f39690
William Rijksen <williamrijksen@gmail.com>
2015-05-28 14:22:49 +0200

    layout optimalisations

--- Views/SensimityTextField.m
+++ Views/SensimityTextField.m
@@ -10,7 +10,7 @@
 - (void)drawRect:(CGRect)rect {
     CALayer *bottomBorder = [CALayer layer];
     bottomBorder.frame = CGRectMake(0.0f, self.frame.size.height - 1, self.frame.size.width, 1.0f);
-    bottomBorder.backgroundColor = [UIColor colorWithRed:0.949 green:0.663 blue:0 alpha:1].CGColor;
+    bottomBorder.backgroundColor = [UIColor colorWithRed:0.255 green:0.259 blue:0.255 alpha:1].CGColor;
     [self.layer addSublayer:bottomBorder];
 }
 
